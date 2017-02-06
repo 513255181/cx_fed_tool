36952e92f10737f97f0e348a8d3c927397ed7945
zhangbingqi <zhangbingqi@email.com>
2017-02-06 18:28:14 +0800

    初版命令行设定

--- bin/fed.js
+++ bin/fed.js
@@ -4,11 +4,15 @@ var program = require('commander');
 
 program
 	.version(require('../package.json').version)
+	.option('-mp,--medium-project','创建你的中型项目')
 	.usage('[options] [project name]')
-	.option('-medium-project','创建你的中型项目')
 	.parse(process.argv);
+	
 
-var pname = program.args[0]
-if(!pname) program.help();
+var pname = program.args;
+if(program.mediumProject){
+	console.log('medium-project');
+}
+if(pname) program.help();
 
 //gs(pname);
